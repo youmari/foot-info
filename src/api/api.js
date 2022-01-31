bfc1fb887526b55a7dd92ae76d068e05a504cd9a
youmari <youmari300@gmail.com>
2022-01-31 20:27:11 +0100

    Set up folder structure and config axios

---
+++ src/api/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const fetchData = axios.create({
+  baseURL: "https://v3.football.api-sports.io/",
+  headers: { "x-rapidapi-key": process.env.REACT_APP_API_TOKKEN },
+});
+
+ 
+export default fetchData;
