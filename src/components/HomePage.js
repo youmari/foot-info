d5abad247981c8825ca20bcfadeb1076412cfba7
youmari <youmari300@gmail.com>
2022-02-02 05:10:58 +0100

    Add search field to filter the categories

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { v4 as uuid } from 'uuid';
 import fetchData from '../api/api';
@@ -6,8 +6,12 @@ import { fetchCountriesStatus, getCountries, getCountriesFailure } from '../redu
 import Country from './Country';
 
 const HomePage = () => {
+  const [searchInput, setSearchInput] = useState('');
   const dispatch = useDispatch();
   const countriesData = useSelector((state) => state.countries.onSuccess);
+  const filtredCountriesData = countriesData?.filter((country) => (
+    country.name.toLowerCase().includes(searchInput.toLocaleLowerCase())
+  ));
   useEffect(async () => {
     if (!countriesData.length) {
       try {
@@ -23,9 +27,12 @@ const HomePage = () => {
 
   return (
     <>
+      <div className="flex justify-center bg-blue-700">
+        <input className="w-13 rounded font-gill text-base p-2 border-solid border-blue-900 text-blue-900 placeholder:text-blue-900" type="search" placeholder="Search by Country..." onChange={(e) => setSearchInput(e.target.value)} />
+      </div>
       <h2 className="font-gill bg-blue-800 text-white text-sm  pl-2">Leagues by Countries</h2>
       <div className="grid grid-cols-2 h-full bg-blue-700">
-        {countriesData?.map((country) => (
+        {filtredCountriesData?.map((country) => (
           <Country key={uuid()} country={country} />
         ))}
       </div>
