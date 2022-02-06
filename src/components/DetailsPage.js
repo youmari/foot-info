f4c30e478b194a921e72b50f9016e2316449892a
youmari <youmari300@gmail.com>
2022-02-06 02:38:16 +0100

    Add loading to detail page

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 import { ArrowLeftIcon, CogIcon, MicrophoneIcon } from '@heroicons/react/solid';
-import { ArrowCircleRightIcon } from '@heroicons/react/outline';
+import { ArrowCircleRightIcon, RefreshIcon } from '@heroicons/react/outline';
 import fetchData from '../api/api';
 import {
   fetchLeagueStatus,
@@ -14,15 +14,15 @@ import {
 const DetailsPage = () => {
   const { countryId } = useParams();
   const dispatch = useDispatch();
-  const leagues = useSelector((state) => state.leagues.onSuccess);
+  const { onSuccess, isLoading } = useSelector((state) => state.leagues);
   useEffect(async () => {
     try {
       dispatch(fetchLeagueStatus({ bool: true }));
       const {
         data: { response },
       } = await fetchData(`leagues?country=${countryId}`);
-      dispatch(fetchLeagueStatus({ bool: false }));
       dispatch(getLeague(response));
+      dispatch(fetchLeagueStatus({ bool: false }));
     } catch (err) {
       dispatch(getLeagueFailure({ err }));
     }
@@ -33,58 +33,67 @@ const DetailsPage = () => {
         <MicrophoneIcon className="w-5 text-white cursor-pointer" />
         <CogIcon className="w-5 text-white m-3 ml-6 mr-3 cursor-pointer" />
       </div>
-      <div className="flex items-center bg-blue-700">
-        <Link to="/" className=" text-white inline">
-          <ArrowLeftIcon className="w-11 h-auto p-3" />
-        </Link>
-        <h2 className="ml-3 text-lg text-white">
-          Details about
-          {'  '}
-          {leagues[0]?.country.name}
-          {'  '}
-          Leagues
-        </h2>
-      </div>
-      <article className="flex justify-around bg-blue-500 p-3">
-        <img
-          className="h-auto w-32 self-center p-1"
-          src={leagues[0]?.country.flag}
-          alt="Country flag"
-        />
-        <div className="flex flex-col justify-end">
-          <h3 className="font-gill font-bold text-white text-xl text-right mr-2">
-            {leagues[0]?.country.name}
-          </h3>
-          <p className="font-lato text-lg text-right text-white mr-2">
-            {leagues[0]?.country.code}
-          </p>
-        </div>
-      </article>
-      <h4 className="bg-blue-800 text-white font-lato text-base pl-2">
-        Leagues and Details about leagues in
-        {' '}
-        {leagues[0]?.country.name}
-      </h4>
-      {leagues?.map((league) => (
-        <div key={league.league.id}>
-          <div className="bg-blue-500">
-            <article
-              className="h-20 flex items-center bg-blue-700 even:bg-blue-900 gap-3 relative"
-              key={league.league.id}
-            >
-              <img
-                className="w-16 h-auto p-3"
-                src={league.league.logo}
-                alt="league logo"
-              />
-              <h3 className="font-lato text-lg text-white">
-                {league.league.name}
-              </h3>
-              <ArrowCircleRightIcon className="w-4 text-white absolute right-3" />
-            </article>
+      {!isLoading ? (
+        <>
+          <div className="flex items-center bg-blue-700">
+            <Link to="/" className=" text-white inline">
+              <ArrowLeftIcon className="w-11 h-auto p-3" />
+            </Link>
+            <h2 className="ml-3 text-lg text-white">
+              Details about
+              {'  '}
+              {onSuccess[0]?.country.name}
+              {'  '}
+              Leagues
+            </h2>
           </div>
+          <article className="flex justify-around bg-blue-500 p-3">
+            <img
+              className="h-auto w-32 self-center p-1"
+              src={onSuccess[0]?.country.flag}
+              alt="Country flag"
+            />
+            <div className="flex flex-col justify-end">
+              <h3 className="font-gill font-bold text-white text-xl text-right mr-2">
+                {onSuccess[0]?.country.name}
+              </h3>
+              <p className="font-lato text-lg text-right text-white mr-2">
+                {onSuccess[0]?.country.code}
+              </p>
+            </div>
+          </article>
+          <h4 className="bg-blue-800 text-white font-lato text-base pl-2">
+            onSuccess and Details about leagues in
+            {' '}
+            {onSuccess[0]?.country.name}
+          </h4>
+          {onSuccess?.map((league) => (
+            <div key={league.league.id}>
+              <div className="bg-blue-500">
+                <article
+                  className="h-20 flex items-center bg-blue-700 even:bg-blue-900 gap-3 relative"
+                  key={league.league.id}
+                >
+                  <img
+                    className="w-16 h-auto p-3"
+                    src={league.league.logo}
+                    alt="league logo"
+                  />
+                  <h3 className="font-lato text-lg text-white">
+                    {league.league.name}
+                  </h3>
+                  <ArrowCircleRightIcon className="w-4 text-white absolute right-3" />
+                </article>
+              </div>
+            </div>
+          ))}
+        </>
+      ) : (
+        <div className="h-full flex flex-col items-center justify-center">
+          <RefreshIcon className="w-14 text-white animate-spin" />
+          <p className="font-lato font-bold text-xl text-white">Loading...</p>
         </div>
-      ))}
+      )}
     </div>
   );
 };
