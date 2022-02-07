2ceed27f59400f4377e6e66cc322995ffde57976
youmari <youmari300@gmail.com>
2022-02-07 18:50:25 +0100

    Refactor header into small component

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+
+const Header = ({ name, flag, code }) => (
+  <article className="flex justify-around bg-blue-500 p-3">
+    <img
+      className="h-auto w-32 self-center p-1"
+      src={flag}
+      alt="Country flag"
+    />
+    <div className="flex flex-col justify-end">
+      <h3 className="font-gill font-bold text-white text-xl text-right mr-2">
+        {name}
+      </h3>
+      <p className="font-lato text-lg text-right text-white mr-2">
+        {code}
+      </p>
+    </div>
+  </article>
+);
+
+Header.propTypes = {
+  name: PropTypes.string.isRequired,
+  flag: PropTypes.string.isRequired,
+  code: PropTypes.string.isRequired,
+};
+
+export default Header;
