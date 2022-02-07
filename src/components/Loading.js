bfd4a7215e20480ddf662cae678864bbc2bc5330
youmari <youmari300@gmail.com>
2022-02-07 18:50:55 +0100

    Refactor loading into small component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,7 @@
 import { RefreshIcon } from '@heroicons/react/outline';
 
 const Loading = () => (
-  <div className="h-full flex flex-col items-center justify-center">
+  <div className="h-screen flex flex-col items-center justify-center">
     <RefreshIcon className="w-14 text-white animate-spin" />
     <p className="font-lato font-bold text-xl text-white">Loading...</p>
   </div>
